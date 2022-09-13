41e72bf9dcd14dbdb20bfd245c283f776bac395a
Tova Jertfelt <tovizitan@gmail.com>
2022-09-13 08:48:28 +0200

    mobilversion och ipadversion klar

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -11,8 +11,6 @@ const checkOption = (id) => {}
 
 const welcomeGIF = "https://cdn.dribbble.com/users/1261045/screenshots/11391612/media/58cd07da8fb87504d054fb1d186abcb0.gif";
 
-
-
   return (
     <div className={styles.homepage__content}>
       {fetchError && <div>{fetchError}</div>}
@@ -69,14 +67,15 @@ const welcomeGIF = "https://cdn.dribbble.com/users/1261045/screenshots/11391612/
             </select>
             
             </span>
-            <span>
-              I det här projektet experimenterar jag ännu mer med map, filter och routers.
-              <br></br>
-              Jag har tittat på parametrar, och hur man kan skicka state via Link.<br></br>
+            <span className={styles.homepage__aboutTXT}>
+
+              <p>I det här projektet experimenterar jag ännu mer med map, filter och routers.
+              Jag har tittat på parametrar, och hur man kan skicka state via Link.
               Jag lärde mig att jag kunde göra en egen hook - useFetch - och har använt den för att slippa upprepa kod. 
               Jag har även tittat på en scroll-funktion, men har inte fått det att fungera.
-              Jag har också tittat på hur jag kan konvertera userId till namn - tex 1 till "Håkan". <br></br>
+              Jag har också tittat på hur jag kan konvertera userId till namn - tex 1 till "Håkan". 
               Jag har också testat CSS-moduler(sass). Jag har tittat väldigt hastigt på att göra hemsidan responsiv.
+              </p>
             </span>
         </div>
         </div>
